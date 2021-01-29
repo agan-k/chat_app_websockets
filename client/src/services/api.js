6338b95e784605772640ddc387b369389f2d9b30
kahil nayton <kahilnayton@gmail.com>
2021-01-29 08:55:47 -0500

    created models and edited migration files to allow for correct relationship between tables

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import store from '../store'
+import { logout } from '../store/actions/auth'
+
 
 const API = axios.create({
   baseURL: 'http://127.0.0.1:3001',
@@ -8,5 +11,27 @@ const API = axios.create({
     "Authorization": `Bearer ${localStorage.getItem('token') || ''}`
   }
 })
+
+// The api response interceptor listens for token expired errors  
+API.interceptors.response.use(
+res => {
+  return res
+  },
+  err => {
+    // because expired tokens will error as 401
+    if (err.response !== 401) {
+      throw err
+    }
+    if (typeof err.response.data.error.name !== 'undefined') {
+      // provided by our jwt package
+      if (err.response.data.error.name === 'TokenExpiredErr') {
+        store.dispatch(logout())
+        throw err
+
+      }
+    }
+
+  }
+)
 
 export default API;
