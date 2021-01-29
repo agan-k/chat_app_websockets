c809b0297552b4bd7706be6ea208407a7bae5b62
kahil nayton <kahilnayton@gmail.com>
2021-01-28 19:13:34 -0500

    prepared the backend for image upload

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,24 +3,32 @@ const sequelize = require("sequelize");
 
 exports.update = async (req, res) => {
 
+  // This is created by multer
+  // It adds a file to our request object
+  if (req.file) {
+    req.body.avatar = req.file.filename
+  }
+
+  // error handling for when an empty string is sent
+  if (typeof req.body.avatar !== 'undefined' && req.body.avatar.length === 0) delete req.body.avatar
+
   try {
-// the rows and results is comes from setting the returning to true
+    // the rows and results is comes from setting the returning to true
     const [rows, result] = await User.update(req.body, {
       where: {
-        id: req.user.id
+        id: req.user.id,
       },
       returning: true, // return an array of updated users
-      individualHooks: true // So that sequelize will execute hooks on this modal
-    })
+      individualHooks: true, // So that sequelize will execute hooks on this modal
+    });
     // Get the plane object and not the sequelize instance by setting plane raw to true
-    const user = result[0].get({ raw: true })
-    user.avatar = result[0].avatar
-    delete user.password // so that it;'s not visible on the front
+    const user = result[0].get({ raw: true });
+    user.avatar = result[0].avatar;
+    delete user.password; // so that it;'s not visible on the front
 
-    return res.send(user)
+    return res.send(user);
   } catch (e) {
-    return res.status(500).json({error: e.message})
+    return res.status(500).json({ error: e.message });
   }
   return res.send("User controller");
 };
-  
---
+++ server/middleware/fileUpload.js
@@ -0,0 +1,72 @@
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+
+
+const getFileType = (file) => {
+  const mimeType = file.mimetype.split('/')
+  return mimeType[mimeType.length - 1]
+}
+
+const generateFileName = (req, file, cb) => {
+  const extension = getFileType(file)
+  
+  // Here we are creating a unique and random file name and add the extension
+  // 1E9 is a short notation for 1 billion
+  const filename = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' + extension
+  
+  cb(null, file.fieldname + '-' + filename)
+}
+
+const fileFilter = (req, file, cb) => {
+  const extension = getFileType(file)
+  
+  // using regex to evaluate the allowed extensions
+    const allowedTypes = /jpeg|jpg|png/
+    
+    const passed = allowedTypes.test(extension)
+
+    if (passed) {
+      return cb(null, true)
+    }
+    // this is how multer expects it to be returned
+    return cb(null, false)
+}
+  
+  exports.userFile = ((req, res, next) => {
+
+  const storage = multer.diskStorage({
+    // we're going to store the files in their own subdirectory inside the uploads tab
+    destination: function (req, file, cb) {
+      const { id } = req.user
+      const dest = `uploads/user/${id}`
+
+      // the fs module allows us to create different methods on our files
+      fs.access(dest, (error) => {
+        if (error) {
+          // if the file doesn't exist
+          return fs.mkdir(dest, (error) => {
+            cb(error, dest)
+          })
+        } else {
+          // it doesn't exist - we need to delete the current avatar since a user can only have one at a time
+          fs.readdir(dest, (error, files) => {
+            if (error) throw error
+
+            for (const file of files) {
+              fs.unlink(path.join(dest, file), error => {
+                if (error) throw error
+              })
+            }
+          })
+          // typical cb structure - first parameter is the error and the second is the response
+          return cb(null, dest)
+        }
+      })
+    },
+    filename: generateFileName
+  })
+
+  return multer({storage, fileFilter}).single('avatar')
+
+})()
