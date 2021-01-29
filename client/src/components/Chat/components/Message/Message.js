512d9584ba76e01a47d666efd2a7e1ad246d19d8
kahil nayton <kahilnayton@gmail.com>
2021-01-29 17:35:07 -0500

    set up the skeloton of the message input component in preparation for sockets

--- client/src/components/Chat/components/Message/Message.js
+++ client/src/components/Chat/components/Message/Message.js
@@ -1,26 +1,37 @@
-import React from 'react'
-import "./Message.scss"
+import React from "react";
+import "./Message.scss";
 
 const Message = ({ user, chat, index, message }) => {
-  
   const determineMargin = () => {
+    if (index + 1 === chat.Messages.length) return;
 
-    if (index + 1 === chat.Messages.length) return
-
-    return message.fromUserId === chat.Messages[index + 1].fromUserId ? 'ms-5' : 'ms-10'
-  }
+    return message.fromUserId === chat.Messages[index + 1].fromUserId
+      ? "ms-5"
+      : "ms-10";
+  };
 
   return (
-    <div className={`message ${determineMargin()} ${message.fromUserId === user.id ? 'creator' : ''}`}>
-      <div className={message.fromUserId === user.id ? 'owner' : 'other-person'}>
-        {
-          message.type === 'text'
-            ? <p className="m-0">{message.message}</p>
-            : <img src={message.message} alt="User upload"/>
-        }
+    <div
+      className={`message ${determineMargin()} ${
+        message.fromUserId === user.id ? "creator" : ""
+      }`}
+    >
+      <div
+        className={message.fromUserId === user.id ? "owner" : "other-person"}
+      >
+        {message.FromUserId !== user.id ? (
+          <h6 className="m-0">
+            {message.User.firstName} {message.User.lastName}
+          </h6>
+        ) : null}
+        {message.type === "text" ? (
+          <p className="m-0">{message.message}</p>
+        ) : (
+          <img src={message.message} alt="User upload" />
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
--- client/src/components/Chat/components/MessageInput/MessageInput.js
+++ client/src/components/Chat/components/MessageInput/MessageInput.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useSelector} from 'react-redux'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import './MessageInput.scss'
 
-const MessageInput = () => {
+const MessageInput = ({chat}) => {
+
+  const user = useSelector(state => state.authReducer.user)
+
+const [message, setMessage] = useState('')
+  const [image, setImage] = useState('')
+  
+  const handleMessage = e => {
+    const value = e.target.value;
+    // Show that you are typing something 
+    setMessage(value)
+
+    // Notify other users that this user is typing
+
+  }
+
+  const handleKeyDown = (e, imageUpload) => {
+    if (e.key === 'enter') setMessage(imageUpload)
+  }
+
+  const sendMessage = (imageUpload) => {
+    if (message.length < 1 && !imageUpload) return;
+
+    const msg = {
+      type: imageUpload ? 'image' : 'text',
+      fromUserId: user.id,
+      toUserId: chat.Users.map(user => user.id),
+      chatId: chat.id,
+      message: imageUpload ? image : message
+    }
+
+    setMessage('')
+    setImage('')
+
+    // send message with socket
+  }
+
   return (
-    <div>
-      Message Input
+    <div className="input-container">
+      <div className="message-input">
+        <input
+          onChange={e => handleMessage(e)}
+          onKeyDown={e => handleKeyDown(e, false)}
+          placeholder="Message... "
+          type="text"
+        />
+        
+        <FontAwesomeIcon
+          icon={['far', 'smile']}
+          className="fa-icon"
+        />
+      </div>
     </div>
   )
 }
